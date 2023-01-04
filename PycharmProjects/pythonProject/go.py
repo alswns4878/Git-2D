360054e0d7639731dbf741dd68a1549b9fc95fd6
alswns4878 <118281560+alswns4878@users.noreply.github.com>
2023-01-04 15:08:20 +0900

    Revert "."
    This reverts commit 211068b5ca504b618689f055923893cb2a1f45c8.

--- PycharmProjects/pythonProject/go.py
+++ PycharmProjects/pythonProject/go.py
@@ -3,19 +3,19 @@ from pico2d import *
 open_canvas(1056, 672)
 grass = load_image("backgroundb1.png")
 character = load_image("image1.png")
-#monster = load_image("Bringer-of-Death_Walk_1.png")
+monster = load_image("Bringer-of-Death_Walk_1.png")
 
 x = 0
 frame =0
 
-while (x < 1055):
+while (x < 1056):
     clear_canvas()
     grass.draw_now(538, 336)
-    #monster.draw_now(900, 160)
+    monster.draw_now(900, 160)
     character.clip_draw(frame * 140, 0, 140, 93, x, 160)
     update_canvas()
     frame = (frame + 1) % 8
     x += 8
     delay(0.05)
-#################
+
 close_canvas()
